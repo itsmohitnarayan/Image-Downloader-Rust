8f81f38854cceee97defcbacb117c5f4ff33699a
Kumar Mohit <mohit724196@gmail.com>
2024-02-02 23:25:07 +0530

    code

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,26 @@ use std::io::copy;
 use std::fs::File;
 use tempfile::Builder;
 
+error_chain!{
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let target = "http::www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        
+}
