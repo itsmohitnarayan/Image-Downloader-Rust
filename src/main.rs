f77879ad5c994e3588c0010dfbfd746abb286219
Kumar Mohit <mohit724196@gmail.com>
2024-02-03 22:24:39 +0530

    Improvement

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,43 @@
 use error_chain::error_chain;
-use std::io::copy;
 use std::fs::File;
+use std::io::copy;
 use tempfile::Builder;
 
-error_chain!{
-    foreign_links{
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
+use log::{info};
+
+error_chain! {
+     foreign_links {
+         Io(std::io::Error);
+         HttpRequest(reqwest::Error);
+     }
+}
+
+fn generate_filename(url: &reqwest::Url) -> String {
+    url.path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("tmp.bin")
+        .to_string()
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::init(); // Initialize logger
+
     let tmp_dir = Builder::new().prefix("example").tempdir()?;
-    let target = "http::www.rust-lang.org/logos/rust-logo-512x512.png";
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+
     let response = reqwest::get(target).await?;
+    let fname = generate_filename(response.url());
 
-    let mut dest = {
-        let fname = response
-            .url()
-            .path_segments()
-            .and_then(|segments| segments.last())
-            .and_then(|name| if name.is_empty() { None } else { Some(name) })
-            .unwrap_or("tmp.bin");
+    info!("file to download: '{}'", fname);
+    let fname = tmp_dir.path().join(fname);
+    info!("will be located under: '{:?}'", fname);
 
-        println!("file to download: '{}'", fname);
-        let fname = tmp_dir.path().join(fname);
-        println!("will be located under: '{:?}'", fname);
-        File::create(fname)?
-    };
+    let mut dest = File::create(fname)?;
+
     let content = response.text().await?;
     copy(&mut content.as_bytes(), &mut dest)?;
+
     Ok(())
 }
